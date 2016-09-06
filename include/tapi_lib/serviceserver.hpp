85f3e91a372d71b240a846df73f55f51b0e6afa9
tobby <tobby@tobby.eu>
2016-09-06 15:09:41 +0200

    Add ServiceServer class to build Tapi-conform ServiceServers

---
+++ include/tapi_lib/serviceserver.hpp
@@ -0,0 +1,34 @@
+#ifndef SERVICESERVER_H
+#define SERVICESERVER_H
+
+#include <string>
+#include <vector>
+#include "ros/advertise_service_options.h"
+#include "ros/node_handle.h"
+#include "ros/service_server.h"
+#include "tapi_client.hpp"
+
+#define ServiceOptionsForTapi(type, callfctn)                                                                          \
+  ros::AdvertiseServiceOptions::create<type>("", boost::bind((callfctn), this, _1, _2), ros::VoidPtr(this), NULL)
+
+namespace Tapi
+{
+class ServiceServer : public TapiClient
+{
+public:
+  // Constructor/Destructor
+  ServiceServer(ros::NodeHandle *nh, std::string nodename = "");
+  ~ServiceServer();
+
+  // Public member functions
+  ros::ServiceServer *AddFeature(ros::AdvertiseServiceOptions opt, std::string featurename = "");
+
+private:
+  // Private member variables
+  ros::NodeHandle *nh;
+  std::string nodename;
+  std::vector<std::pair<ros::AdvertiseServiceOptions, ros::ServiceServer *>> services;
+};
+}
+
+#endif  // SERVICESERVER_H
---
+++ src/serviceserver.cpp
@@ -0,0 +1,49 @@
+#include "include/tapi_lib/serviceserver.hpp"
+
+using namespace std;
+
+namespace Tapi
+{
+// Constructor/Destructor
+
+ServiceServer::ServiceServer(ros::NodeHandle* nh, string nodename)
+  : TapiClient(nh, nodename, PUBLISHER_DEVICE), nh(nh), nodename(nodename)
+{
+}
+
+ServiceServer::~ServiceServer()
+{
+  for (int i = 0; i < services.size(); i++)
+  {
+    services[i].second->shutdown();
+    delete services[i].second;
+    services[i].second = 0;
+  }
+}
+
+// Public member functions
+
+ros::ServiceServer* ServiceServer::AddFeature(ros::AdvertiseServiceOptions opt, string featurename)
+{
+  std::string featureUUID = getNextFeatureUUID();
+  ros::ServiceServer* service = 0;
+  std::string serviceName = "/Tapi/" + uuid + "/" + featureUUID;
+  opt.service = serviceName;
+
+  service = new ros::ServiceServer(nh->advertiseService(opt));
+
+  if (service)
+  {
+    pair<ros::AdvertiseServiceOptions, ros::ServiceServer*> newEntry;
+    newEntry = make_pair(opt, service);
+    services.push_back(newEntry);
+    tapi_lib::Feature feature;
+    string temp = opt.datatype;
+    feature.FeatureType = temp;
+    feature.Name = featurename;
+    feature.UUID = featureUUID;
+    featureMsgs.push_back(feature);
+  }
+  return service;
+}
+}
