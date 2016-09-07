1b969cd829a848cedf358af4b0cd5fc4b21a864d
tobby <tobby@tobby.eu>
2016-09-07 14:26:20 +0200

    One library to include full tapi_lib

---
+++ include/tapi_lib/tapi_lib.hpp
@@ -0,0 +1,26 @@
+// Server-Libraries
+#include "connection.hpp"
+#include "device.hpp"
+#include "feature.hpp"
+
+// Client-Libraries
+#include "publisher.hpp"
+#include "serviceclient.hpp"
+#include "serviceserver.hpp"
+#include "subscriber.hpp"
+
+// "Helper"-Library
+#include "tapi_client.hpp"
+
+// Messages
+#include "tapi_lib/Connect.h"
+#include "tapi_lib/Connection.h"
+#include "tapi_lib/Device.h"
+#include "tapi_lib/Feature.h"
+#include "tapi_lib/GetConnectionList.h"
+#include "tapi_lib/GetDeviceList.h"
+#include "tapi_lib/GetStateMachine.h"
+#include "tapi_lib/Hello.h"
+#include "tapi_lib/State.h"
+#include "tapi_lib/Transition.h"
+
